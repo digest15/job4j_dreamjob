b90f9a90a82d0a67227339bbecceffb71ab0c614
digest15 <digest15@yandex.ru>
2023-02-04 18:01:51 +0800

    #504842
    Implement candidates

---
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -0,0 +1,59 @@
+package ru.job4j.dreamjob.repository;
+
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryCandidateRepository implements CandidateRepository {
+    private static final MemoryCandidateRepository INSTANCE = new MemoryCandidateRepository();
+
+    private int nextId = 1;
+
+    private final Map<Integer, Candidate> candidates = new HashMap<>();
+
+    private MemoryCandidateRepository() {
+        save(new Candidate(0, "Иванов", "Java 6 yeas"));
+        save(new Candidate(1, "Петров", "Java 3 yeas"));
+        save(new Candidate(2, "Сидоров", "Java 2 yeas"));
+        save(new Candidate(3, "Баширов", "Like Salisbury"));
+        save(new Candidate(4, "Чипига", "Like spires"));
+        save(new Candidate(5, "Арсентьев", "Like job4j"));
+    }
+
+    public static MemoryCandidateRepository getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Candidate save(Candidate candidate) {
+        candidate.setId(nextId++);
+        this.candidates.put(candidate.getId(), candidate);
+        return candidate;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return candidates.remove(id) != null;
+    }
+
+    @Override
+    public boolean update(Candidate candidate) {
+        return candidates.computeIfPresent(candidate.getId(), (id, oldVacancy) ->
+                new Candidate(oldVacancy.getId(),
+                        candidate.getName(),
+                        candidate.getDescription())) != null;
+    }
+
+    @Override
+    public Optional<Candidate> findById(int id) {
+        return Optional.ofNullable(candidates.get(id));
+    }
+
+    @Override
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
